dff760eb2e5af54bcedaaefaa16b274648b9fd32
Hasin Hayder <countdraculla@gmail.com>
2023-11-30 09:34:43 +0600

    Mock Auth Markup

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=600" alt="Your Company">
+    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=emerald&shade=400" alt="Your Company">
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
   </div>
 
@@ -22,7 +22,7 @@
         <div class="flex items-center justify-between">
           <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           <div class="text-sm">
-            <a href="#" class="font-semibold text-green-600 hover:text-green-500">Forgot password?</a>
+            <a href="#" class="font-semibold text-emerald-500 hover:text-emerald-600">Forgot password?</a>
           </div>
         </div>
         <div class="mt-2">
@@ -31,13 +31,13 @@
       </div>
 
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
       </div>
     </form>
 
     <p class="mt-10 text-center text-sm text-gray-500">
       Not a member?
-      <a href="#" class="font-semibold leading-6 text-green-600 hover:text-green-500">Start a 14 day free trial</a>
+      <a href="#" class="font-semibold leading-6 text-emerald-500 hover:text-emerald-500">Start a 14 day free trial</a>
     </p>
   </div>
 </div>
--- src/views/SecretView.vue
+++ src/views/SecretView.vue
@@ -8,8 +8,8 @@
         </div>
         <div class=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
             <div class="mb-8">
-                <p class="text-sm text-gray-600 flex items-center mb-2">
-                    <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <p class="text-sm flex items-center mb-2 text-gray-400">
+                    <svg class="fill-current text-emerald-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
                     </svg>
                     Welcome John Doe
