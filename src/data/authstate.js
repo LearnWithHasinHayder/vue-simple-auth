f20098c464248e9fcbc694a2cdb8957e3d6bb934
Hasin Hayder <countdraculla@gmail.com>
2023-12-01 09:39:35 +0600

    Episode 04

--- src/data/authstate.js
+++ src/data/authstate.js
@@ -7,8 +7,19 @@ const AuthState = reactive({
     username: null,
     password: null,
     login(){
-        console.log({username: this.username, password: this.password})
-    }
+        // console.log({username: this.username, password: this.password})
+        axios.post('http://localhost:8001/index.php',{
+            email: this.username,
+            password: this.password
+        }).then((response) => {
+            if(response.data.success==1){
+                this.isAuthenticated = true
+                this.user = response.data
+                this.password=null
+                this.username=null
+            }
+        })
+    },
 });
 
 export default AuthState;
