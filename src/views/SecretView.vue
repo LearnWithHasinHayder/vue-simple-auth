94105a2a776e18e08f9f0edc40e7f8347c541156
Hasin Hayder <countdraculla@gmail.com>
2023-12-01 09:49:55 +0600

    Episode 06

--- src/views/SecretView.vue
+++ src/views/SecretView.vue
@@ -12,7 +12,7 @@ import AuthState from '../data/authstate';
                     <svg class="fill-current text-emerald-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
                     </svg>
-                    Welcome John Doe
+                    Welcome {{ AuthState.user.name }}
                 </p>
                 <div class="text-gray-900 font-bold text-xl mb-2">Carpe Diem</div>
                 <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
